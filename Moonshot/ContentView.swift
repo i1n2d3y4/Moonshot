7813e5c3a9b057fa767c9b84d8a1937fe9f9a568
VeeBee <vikas@triamis.com>
2023-07-15 18:58:39 +0800

    support generic types

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Missions] = Bundle.main.decode("missions.json")
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(astronauts.count)")
         }
         .padding()
     }
