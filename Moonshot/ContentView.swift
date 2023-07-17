81004793e1ed8b68456aff76ef9f93a116b9103e
VeeBee <vikas@triamis.com>
2023-07-17 18:19:18 +0800

    All challenges complete.
    1. Added a navigation bar button to switch from grid view to list view, with @State private var displayStyleListOrGrid
    2. added gridLayout struct with content to generate a grid
    2. added listLayout struct with code to generate a list. replaced scrollview with List.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,9 +7,101 @@
 
 import SwiftUI
 
+struct gridContent: View {
+    let missions: [Missions]
+    let astronauts: [String: Astronaut]
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.imageName)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundColor(.white.opacity(0.5))
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                            )
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            
+        }
+    }
+    
+}
+
+struct listContent: View {
+    let missions: [Missions]
+    let astronauts: [String: Astronaut]
+    
+    var body: some View {
+        List {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    VStack {
+                        Image(mission.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .padding()
+                        VStack {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundColor(.white.opacity(0.5))
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(.lightBackground)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    )
+                }
+            }
+        }
+        .padding([.horizontal, .bottom])
+        .listStyle(.plain)
+        .listRowBackground(Color.darkBackground)
+    }
+    
+}
+
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Missions] = Bundle.main.decode("missions.json")
+    @State private var displayStyleListOrGrid = "Grid"
     
     let columns = [
         GridItem(.adaptive(minimum: 150))
@@ -17,44 +109,27 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView { 
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.imageName)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                                )
-                        }
-                    }
+            Group {
+                if displayStyleListOrGrid == "Grid" {
+                    gridContent(missions: missions, astronauts: astronauts)
+                } else {
+                    listContent(missions: missions, astronauts: astronauts)
                 }
-                .padding([.horizontal, .bottom])
-                
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                Button {
+                    if displayStyleListOrGrid == "Grid" {
+                        displayStyleListOrGrid = "List"
+                    } else {
+                        displayStyleListOrGrid = "Grid"
+                    }
+                } label: {
+                    Text(displayStyleListOrGrid == "Grid" ? "List": "Grid")
+                }
+            }
         }
     }
 }
